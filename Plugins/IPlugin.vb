161618cda120d00678ea896dca66e773500e6597
Barret <format.e.drive@gmail.com>
2017-12-17 02:53:35 +0100

    Add files via upload

--- Plugins/IPlugin.vb
+++ Plugins/IPlugin.vb
@@ -2,7 +2,7 @@
 
 Namespace Plugins
     Public Interface IPlugin
-        Sub Load()
+        Sub Load(Listener As Listener)
         Sub ClientRequest(Client As Client, ByRef Claimed As Boolean)
         Sub ClientSend(Client As Client, ByRef buffer() As Byte, ByRef ContentType As String)
         ReadOnly Property Name As String
